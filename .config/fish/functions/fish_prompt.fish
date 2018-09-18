38367f2997a542ee5ca580b3a9413c920a6e5a46
Tom Watson <tom@tomjwatson.com>
2018-09-18 17:40:19 +0200

    reflect 0 in '>' in fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,6 +1,16 @@
 function fish_prompt
-    set_color $fish_color_cwd
-    echo -n (prompt_pwd)
+  set prev_status $status
+
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+
+  if test $prev_status -ne 0
+    set_color $fish_color_error
+  else
     set_color normal
-    echo -n ' > '
+  end
+
+  echo -n ' > '
+
+  set_color normal
 end
