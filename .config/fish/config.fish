b99239e4001bfeb4ab03ca02a18682a20a85f707
Tom Watson <tom@tomjwatson.com>
2018-09-19 16:48:10 +0200

    add alt function that creates notification with and says msg

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,11 @@ function smartresize
   mogrify -path $3 -filter Triangle -define filter:support=2 -thumbnail $2 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB $1
 end
 
+function alt
+  /usr/bin/osascript -e "display notification with title \"$argv\""
+  say $argv
+end
+
 # Set up Android Studio
 export ANDROID_HOME=$HOME/Library/Android/sdk
 set -gx PATH $ANDROID_HOME/tools $ANDROID_HOME/platform-tools $PATH
